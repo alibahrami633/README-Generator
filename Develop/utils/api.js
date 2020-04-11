6a166275aa1c956acb300bb6784889f934ec25d1
Aliagho <ali.bahrami633@yahoo.com>
2020-04-11 13:26:07 +1000

    changed the token to env public token

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,43 +1,35 @@
 const dotenv = require('dotenv').config();
 const axios = require("axios");
 
-const key = "e5792a790a0959e57956359d90c34d0d6aff3c0d";
+const token = (process.env.GITHUB_TOKEN == undefined) ? '' : `?access_token=${process.env.GITHUB_TOKEN}`;
 
 class User {
   constructor(name, email, picUrl) {
     this.name = (name && name.trim() !== "") ? name : console.log("name cannot be empty");
-    this.email = (email) ? email : "";
+    this.email = (email) ? email : "No access";
     this.picUrl = picUrl ? picUrl : "";
   }
 }
 
-const api = {
+/** 
+ * @param {username} username 
+ * @return {User}
+ */
+async function getUser(username) {
 
-  /** 
-   * @param {username} username 
-   * @return {User}
-   */
-  async getUser(username) {
+  try {
+    const userUrl = `https://api.github.com/users/${username}${token}`;
+    const response = await axios.get(userUrl);
 
-    try {
-      const userUrl = `https://api.github.com/users/${username}?access_token=${key}`;
-      const response = await axios.get(userUrl);
-      console.log(response.data.avatar_url);
+    if (response == undefined || !("data" in response)
+      || !("repos_url" in response.data))
+      return null;
 
-      if (response.data) {
-        const newUser = new User(username, response.data.email, response.data.avatar_url);
-        // console.log(newUser.avatar_url);
-        return newUser;
-      }
-      else return null;
-    }
-    catch (err) {
-      console.log("user was not found...");
-    }
+    return new User(username, response.data.email, response.data.avatar_url);
   }
-};
+  catch (err) {
+    console.log("user was not found...");
+  }
+}
 
-const user1 = api.getUser("alibahrami633");
-console.log(user1.avatar_url);
-
-module.exports = api.getUser;
+module.exports = getUser;
